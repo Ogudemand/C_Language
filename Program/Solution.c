243d7a4aba6d62e2c6b1afc2b7e30a5607da6d78
Ogudemand <uwhwjwnxbxuw@gmail.com>
2024-01-16 21:47:34 +0900

    (2024.01.16) 차원 배열

--- Program/Solution.c
+++ Program/Solution.c
@@ -1,99 +1,98 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <conio.h> // Key
-#include <windows.h> // Coordinate
+#include <string.h>
 
-#define UP 72
-#define LEFT 75
-#define RIGHT 77
-#define DOWN 80
 
-void GotoXY(int x, int y) 
+
+#define WIDTH 11
+#define HEIGHT 11
+
+char maze[WIDTH][HEIGHT];
+
+void CreateMaze()
 {
-    // x, y 좌표 설정
-    COORD position = { (SHORT)x, (SHORT)y };
+	// 0 : 빈 공간 (" ")
+	// 1 : 벽 (ㅁ)
+	// 2 : 탈출구 (*)
 
-    // 커서 이동 함수
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+	strcpy(maze[0], "1111111111");
+	strcpy(maze[1], "1100111101");
+	strcpy(maze[2], "1110111001");
+	strcpy(maze[3], "1110011011");
+	strcpy(maze[4], "1111010011");
+	strcpy(maze[5], "1100000111");
+	strcpy(maze[6], "1101111111");
+	strcpy(maze[7], "1100000111");
+	strcpy(maze[8], "1110011111");
+	strcpy(maze[9], "1111011111");
+	strcpy(maze[10],"1111111111");
 }
+
+void Render()
+{
+		for (int y = 0; y < HEIGHT; y++) 
+		{
+			for (int x = 0; x < WIDTH; x++) 
+			{
+				char cell = maze[y][x];
+				if (cell == '1') 
+				{
+					printf("■"); 
+				}
+				else if (cell == '0') 
+				{
+					printf(" ");
+				}
+			}
+			printf("\n");
+		}
+}
+
+
+
+typedef struct Student
+{
+	char name[10];
+}Student;
 
 int main()
 {
-    int x = 2;
-    int y = 5;
+		CreateMaze(); 
+		Render();     
 
-	char key = 0;
-
-
-
-    while (1) 
-    {
-        if (_kbhit())
-        {
-            key = _getch();
-
-         
-
-            if (key == -32)
-            {
-                key = _getch();
-            }
-        }
-
-        GotoXY(x, y); 
-        printf(" ");
-
-        switch (key) {
-        case UP:
-            if (y > 0) y--;
-            break;
-        case LEFT:
-            if (x > 1) x -= 2;
-            break;
-        case RIGHT:
-            x += 2;
-            break;
-        case DOWN:
-            y++;
-            break;
-        }
-
-        GotoXY(x, y);
-        printf("*");
-
-    }
-
-//while (1)
-//{
-//	if (_kbhit()) //키보드 입력 확인(true/false)
-//			{
-//		key = _getch(); // key 입력을 받아주는 함수
-//		
-//		if(key == -32)
-//		{
-//			key = _getch();
-//		}
-//
-//		switch (key)
-//		{
-//		case UP: printf("UP\n");
-//			break;
-//		case LEFT: printf("LEFT\n");
-//			break;
-//		case RIGHT: printf("RIGHT\n");
-//			break;
-//		case DOWN: printf("DOWN\n");
-//			break;
-//		}
-//
-//
-//
-//	}
-//
-//	}
-
-	return 0;
-
+return 0;
 }
+
+
+
+
+
+
+
+//int main()
+//{
+//	Student student;
+//
+//	student.name;
+//
+//	// 첫 번쨰 매개변수
+//	// 복사받을 문자 배열을 넣어줍니다.
+//
+//	// 두 번째 매개변수
+//	// 복사시킬 문자열을 넣어줍니다.
+//	strcpy(student.name, "KTM");
+//
+//	printf("student.name의 값 : %s\n", student.name);
+//
+//	strcpy(student.name, "G");
+//
+//	printf("student.name의 값 : %s\n", student.name);
+//
+//	return 0;
+//}
+
+	
+
+
 
 
